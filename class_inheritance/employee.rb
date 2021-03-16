1930e049692ac9d32d8313f41f1b19ca26a4ac48
juliajykim <jkim24@gwu.edu>
2021-03-16 13:49:22 -0400

    Add Manager,Employee#bonus , Manager#add_employee

--- class_inheritance/employee.rb
+++ class_inheritance/employee.rb
@@ -1,9 +1,12 @@
 class Employee
+    attr_reader :salary
    def initialize(name,title,salary,boss)
-    @name,@title,@salary,@boss = name,title,salary,boss
+        @name,@title,@salary,@boss = name,title,salary,boss
    end
 
    def bonus(multiplier)
-    
+    salary * multiplier
    end
+   
+
 end
--- class_inheritance/manager.rb
+++ class_inheritance/manager.rb
@@ -1,5 +1,18 @@
 require_relative "employee"
 
-class Manager
-    
+class Manager < Employee
+    attr_reader :managed_employees    
+    def initialize(name,title,salary,boss)
+        super(name,title,salary,boss)
+        @managed_employees = []
+    end
+
+    def bonus(multiplier)
+        managed_employees.map(&:salary).sum * multiplier
+    end
+
+    def add_employee(employee)
+        managed_employees << employee
+    end
 end
+
