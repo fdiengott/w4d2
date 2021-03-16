6089a4c295d5324c519af9436440ccccc0bbe723
juliajykim <jkim24@gwu.edu>
2021-03-16 14:40:11 -0400

    Add Board#setup_board

--- chess/board.rb
+++ chess/board.rb
@@ -5,7 +5,26 @@ class Board
   attr_reader :board
 
   def initialize
-    @board = Array.new(8) { Array.new(8) }  # fill with ?
+    @board = setup_board # fill with ?
+  end
+
+  def setup_board
+    #4 kn, R, B
+    #2 Q K
+    #16 pwn
+    #16 null_p
+    knight_pos = [[0,1],[0,6],[7,1],[7,6]]
+    black_knights = [Knight.new(:black,),Knight.new(:black,)]
+    knights = 4.times.map do |i|
+      if i < 2
+        Knight.new(:black) #TODO : more args later 
+      else
+        Knight.new(:white) #TODO : more args later 
+      end
+    end
+
+    Array.new(8) { Array.new(8) }
+
   end
 
   def [](pos)
