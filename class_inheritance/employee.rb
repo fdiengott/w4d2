f039b073312466c036bddd052998c0e0f131ec8a
juliajykim <jkim24@gwu.edu>
2021-03-16 12:24:53 -0400

    Add initialize

---
+++ class_inheritance/employee.rb
@@ -0,0 +1,9 @@
+class Employee
+   def initialize(name,title,salary,boss)
+    @name,@title,@salary,@boss = name,title,salary,boss
+   end
+
+   def bonus(multiplier)
+    
+   end
+end
---
+++ class_inheritance/manager.rb
@@ -0,0 +1,5 @@
+require_relative "employee"
+
+class Manager
+    
+end
